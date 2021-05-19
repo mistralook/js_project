beef85d288d74391caaafa904ab82183f56dd248
SergeyPishchulov <lcmrb26@bk.ru>
2021-05-19 23:24:36 +0500

    delete serv_lead.js

--- serv_lead.js
+++
@@ -1,7 +0,0 @@
-app.post('/postResults', (req, res) => {
-    const j=req.json();
-    const userName=j["name"];
-    const score=j["score"];
-    await db.none(`INSERT INTO leaderboard (name, score) VALUES (${userName}, ${score}) ON CONFLICT (name) DO UPDATE  score = leaderboard.score + EXCLUDED.score;`)
-    res.sendFile(__dirname + '/leaderboard.html')
-})
