595857b7afe91b6aea4ec2121f8bb3af7f7e72a7
Knyazzini <knyazzev.lgor@gmail.com>
2021-05-20 11:50:25 +0500

    shuffle questions

--- public/QuestionGenerator.js
+++ public/QuestionGenerator.js
@@ -1,5 +1,15 @@
 import Question from './Question.js';
 
+function shuffle(arr){
+    let j, temp;
+    for(let i = arr.length - 1; i > 0; i--){
+        j = Math.floor(Math.random()*(i + 1));
+        temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+    return arr;
+}
 
 class QuestionGenerator {
     async generateQuestions() {
@@ -8,7 +18,10 @@ class QuestionGenerator {
         let data = await response.json();
         console.log(data);
         for (let jq of data) {
-            const curq = new Question(jq["phrase"], jq["vars"], jq["cor_ind"], jq["price"]);
+            const rightAnswer = jq["vars"][jq["cor_ind"]]
+            const vars = shuffle(jq["vars"])
+            const rightIndex = vars.indexOf(rightAnswer)
+            const curq = new Question(jq["phrase"], vars, rightIndex, jq["price"]);
             resultQuestions.push(curq);
         } 
         return resultQuestions;
